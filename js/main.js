0bd9268d1ac1e07ef1534c262237cb0bd81a34a2
leamontoya19 <leamontoya19@gmail.com>
2023-11-24 22:49:56 +0100

    modified:   index.html 	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -10,44 +10,30 @@
 
 
 
-const buttons = document.querySelectorAll("[dataCarouselButton]");
-console.log(buttons);
+const buttons = document.querySelectorAll("[dataCarouselButton]")
+
 
 buttons.forEach(button => {
-   button.addEventListener ("click", () =>{
-    const offset = button.dataset.carroselButton === "next" ? 1: -1; //El signo de interrogación se utiliza para implementar una expresión condicional conocida como operador ternario. En este caso, la expresión evalúa si el valor de la propiedad "dataset.carroselButton" del botón es igual a "next". Si es así, se asigna el valor de 1 a la constante "offset";
+   button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1 
     const slides = button
-    .closest ("[dataCarousel]")
-    .querySelector ("[dataSlides]")
+    .closest("[dataCarousel]")
+    .querySelector("[dataSlides]")
 
-    const activeSlide = slides.querySelector( "[dataActive]")
-    console.log(activeSlide);
+    const activeSlide = slides.querySelector("[dataActive]")
+    
     //to make sure that it's going to be a continuous loop of images
     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
 
-    console.log(activeSlide);
 
-   });
+   })
   
-});
+})
 
-//----Validar email-----
-/*
-function validationEmail(correo){
-   let expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-   let validEmail = expReg.test(correo);
-
-
-   if(validEmail==true){   
-   }else{
-       alert("This email is invalid")
-   }
-
-}*/
 
 
